cb5dba5c14d0fc1c4894f7c3c7978b5aad6808b6
Denis Monich <denis1989@bk.ru>
2017-11-17 18:03:18 +0300

    fiveth

---
+++ homeWorkC/homeWorkC.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function randomDiap(n,m) {
+    return Math.floor(Math.random()*(m-n+1))+n;
+}
+
+function mood(colorsCount) {
+    var colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
+    var uniqColors={};
+    console.log( 'цветов: ' + colorsCount );
+    for ( var i=1; i<=colorsCount; i++ ) {
+        var n=randomDiap(1,7);
+        uniqColors[colors[n]]='';
+    }
+    for (var key in uniqColors)
+    console.log(key);
+}
+
+mood(5);
