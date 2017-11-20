e201d7441aa72fd5b447b7276698507241ba7c7d
Denis Monich <denis1989@bk.ru>
2017-11-20 18:59:30 +0300

    changed persons

--- persons/persons.js
+++ persons/persons.js
@@ -8,7 +8,7 @@ class Person {
     }
 
     sayHello() {
-        var personType=this instanceof Men;
+        var personType = this instanceof Men;
         var s = 'Hi, my name is ' + this._name;
         if (this._friends.length) {
             s = s + '\nMy friends:';
@@ -21,69 +21,84 @@ class Person {
             }
         }
         if (this._spouse) {
-            if(this instanceof Men){
+            if (this instanceof Men) {
                 s = s + "\nСупруга " + this._spouse._name;
-            }else{
+            } else {
                 s = s + "\nСупруг " + this._spouse._name;
             }
 
         }
         console.log(s);
     }
+
+    hasFriend(friend) {
+        if (this._friends) {
+            for (let i = 0; i < this._friends.length; i++) {
+                if (friend === this._friends[i]) {
+                    return true;
+                }
+            }
+        }
+    }
+
     setFriend(friend) {
-        this._friends.push(friend);
-        friend._friends.push(this);
+        if (!this.hasFriend(friend)) {
+            this._friends.push(friend);
+            friend.setFriend(this);
+        }
     }
 }
 
 class Men extends Person {
     constructor(name) {
         super(name);
-        this._footballTeam=[];
+        this._footballTeam = [];
     }
 
     getMarry(person) {
         if (!this._spouse && person instanceof Women) {
             this._spouse = person;
-            person._spouse=this;
+            person._spouse = this;
         }
     }
-    addToTeam(player){
-        if(this._footballTeam.indexOf(player)<0 && player instanceof Men){
+
+    addToTeam(player) {
+        if (this._footballTeam.indexOf(player) < 0 && player instanceof Men) {
             this._footballTeam.push(player);
             player._footballTeam.push(this);
-        }else{
+        } else {
             console.log('woman or yet play');
         }
     }
 
-    showTeam(){
-        var s='';
-        for (var i=0;i<this._footballTeam.length;i++){
-            s=s+" "+this._footballTeam[i]._name;
+    showTeam() {
+        var s = '';
+        for (var i = 0; i < this._footballTeam.length; i++) {
+            s = s + " " + this._footballTeam[i]._name;
         }
-        console.log('Team of '+ this._name+': '+s);
+        console.log('Team of ' + this._name + ': ' + s);
     }
 }
 
 class Women extends Person {
     constructor(name) {
         super(name);
-        this._nailColor=null;
+        this._nailColor = null;
     }
 
     getMarry(person) {
         if (!this._spouse && person instanceof Men) {
             this._spouse = person;
-            person._spouse=this;
+            person._spouse = this;
         }
     }
-    setNailColor(color){
-        this._nailColor=color;
+
+    setNailColor(color) {
+        this._nailColor = color;
     }
 
-    showNailColor(){
-        console.log('Nail color of '+this._name+' is '+ this._nailColor);
+    showNailColor() {
+        console.log('Nail color of ' + this._name + ' is ' + this._nailColor);
     }
 }
 
