3fc3517e74a9d0087ebbca3dcb0ebe0cb3b0b507
Denis Monich <denis1989@bk.ru>
2017-11-21 09:29:39 +0300

    changed printDom

--- printDom/printDom.js
+++ printDom/printDom.js
@@ -5,8 +5,7 @@ function printChilds(tag,s) {
     s=s+' ';
     let childs=tag.childNodes;
         for (let i=0;i<childs.length;i++){
-            if(childs[i].nodeName==='#text')continue;//чтобы не захломлять консоль))
-            console.log(s+childs[i].nodeName);
+            if(childs[i].nodeType==1)console.log(s+childs[i].nodeName);
             if (childs[i].hasChildNodes()){
                 printChilds(childs[i],s+' ');
             }
