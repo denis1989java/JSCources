9b0b66c404e824a17915a0aa0f6f03bf41136d1f
Denis Monich <denis1989@bk.ru>
2017-11-21 09:08:56 +0300

    added printDom

---
+++ printDom/printDom.js
@@ -0,0 +1,15 @@
+let body = document.getElementById('body');
+let s = '';
+console.log(s+body.nodeName);
+function printChilds(tag,s) {
+    s=s+' ';
+    let childs=tag.childNodes;
+        for (let i=0;i<childs.length;i++){
+            if(childs[i].nodeName==='#text')continue;//чтобы не захломлять консоль))
+            console.log(s+childs[i].nodeName);
+            if (childs[i].hasChildNodes()){
+                printChilds(childs[i],s+' ');
+            }
+        }
+}
+printChilds(body,s);
